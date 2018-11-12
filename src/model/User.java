c8e9722bd4f9f855be106393f518e0844b44eda2
sfinias <sfinias@hotmail.com>
2018-11-13 01:47:51 +0200

    Added User entity From Nah

--- src/model/User.java
+++ src/model/User.java
@@ -1,10 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package model;
 
-import javax.persistence.*;
-import java.util.Arrays;
-import java.util.Objects;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ *
+ * @author User
+ */
 @Entity
+@Table(name = "user")
+@XmlRootElement
 @NamedQueries({
         @NamedQuery(name = "User.findAll", query = "SELECT u FROM User u")
         , @NamedQuery(name = "User.findByEmail", query = "SELECT u FROM User u WHERE u.email = :email")
@@ -13,17 +31,45 @@ import java.util.Objects;
         , @NamedQuery(name = "User.findByLastName", query = "SELECT u FROM User u WHERE u.lastName = :lastName")
         , @NamedQuery(name = "User.findByAddress", query = "SELECT u FROM User u WHERE u.address = :address")
         , @NamedQuery(name = "User.findByPhone", query = "SELECT u FROM User u WHERE u.phone = :phone")})
-public class User {
-    private String email;
-    private String pass;
-    private String firstName;
-    private String lastName;
-    private String address;
-    private String phone;
-    private byte[] photo;
+public class User implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     @Id
     @Column(name = "email")
+    private String email;
+    @Basic(optional = false)
+    @Column(name = "pass")
+    private String pass;
+    @Basic(optional = false)
+    @Column(name = "first_name")
+    private String firstName;
+    @Basic(optional = false)
+    @Column(name = "last_name")
+    private String lastName;
+    @Basic(optional = false)
+    @Column(name = "address")
+    private String address;
+    @Column(name = "phone")
+    private String phone;
+    @Lob
+    @Column(name = "photo")
+    private byte[] photo;
+
+    public User() {
+    }
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public User(String email, String pass, String firstName, String lastName, String address) {
+        this.email = email;
+        this.pass = pass;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
     public String getEmail() {
         return email;
     }
@@ -32,8 +78,6 @@ public class User {
         this.email = email;
     }
 
-    @Basic(optional = false)
-    @Column(name = "pass")
     public String getPass() {
         return pass;
     }
@@ -42,8 +86,6 @@ public class User {
         this.pass = pass;
     }
 
-    @Basic(optional = false)
-    @Column(name = "first_name")
     public String getFirstName() {
         return firstName;
     }
@@ -52,8 +94,6 @@ public class User {
         this.firstName = firstName;
     }
 
-    @Basic(optional = false)
-    @Column(name = "last_name")
     public String getLastName() {
         return lastName;
     }
@@ -62,8 +102,6 @@ public class User {
         this.lastName = lastName;
     }
 
-    @Basic(optional = false)
-    @Column(name = "address")
     public String getAddress() {
         return address;
     }
@@ -72,8 +110,6 @@ public class User {
         this.address = address;
     }
 
-    @Basic
-    @Column(name = "phone")
     public String getPhone() {
         return phone;
     }
@@ -82,9 +118,6 @@ public class User {
         this.phone = phone;
     }
 
-    @Basic
-    @Lob
-    @Column(name = "photo")
     public byte[] getPhoto() {
         return photo;
     }
@@ -94,15 +127,27 @@ public class User {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        User user = (User) o;
-        return Objects.equals(getEmail(), user.getEmail());
+    public int hashCode() {
+        int hash = 0;
+        hash += (email != null ? email.hashCode() : 0);
+        return hash;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(getEmail());
+    public boolean equals(Object object) {
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.email == null && other.email != null) || (this.email != null && !this.email.equals(other.email))) {
+            return false;
+        }
+        return true;
     }
+
+    @Override
+    public String toString() {
+        return "model.User[ email=" + email + " ]";
+    }
+
 }
