51891b78bc06a2fb33195615ac9d3b69498745e1
sfinias <sfinias@hotmail.com>
2018-11-12 13:35:32 +0200

    Test Init

--- MainTest.java
+++ MainTest.java
@@ -1,5 +1,5 @@
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("Display AAAAAAAAAAAAAAAAAAAA");
+        System.out.println("Dasdasfasd");
     }
 }
