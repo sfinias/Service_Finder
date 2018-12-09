45c44644b30a586ff2ebb24ccae22684721193e0
Nah <anermis@gmail.com>
2018-12-09 20:34:50 +0200

    adding the full template!

---
+++ src/main/java/dao/ProfessionsDAOInterface.java
@@ -0,0 +1,16 @@
+package dao;
+
+
+import java.util.List;
+import model.ProfessionsEntity;
+
+/**
+ *
+ * @author Nah
+ */
+
+
+public interface ProfessionsDAOInterface {
+    public List<ProfessionsEntity> getAllProfessions();
+   
+}
