9b0f866af1b07577a2e76f6e4bcc1ca36ab46ce4
Ioannis Sfinias <sfinias@hotmail.com>
2018-12-16 19:19:31 +0200

    Added filter

--- src/main/java/utils/PageFilter.java
+++ src/main/java/utils/PageFilter.java
@@ -13,24 +13,25 @@ public class PageFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse res = (HttpServletResponse) servletResponse;
 
-        RegisterEntity user = (RegisterEntity)req.getSession().getAttribute("user");
-//        System.out.println(req.getRequestURI());
-//        System.out.println("Query String: " + req.getQueryString() );
-        System.out.println("servletPath: " + req.getServletPath() );
-//        System.out.println("Context PATH: " + req.getContextPath() );
-//        System.out.println("GET METHOD: " + req.getMethod());
-//        System.out.println("Get request URL: " + req.getRequestURL() );
-//        System.out.println("Get path translated: " + req.getPathTranslated() );
+            RegisterEntity user = (RegisterEntity)req.getSession().getAttribute("user");
 
+//        if(user == null && !(req.getServletPath().startsWith("/home") || req.getServletPath().startsWith("/verification") || req.getServletPath().equals("/index.jsp"))) {
+//            res.sendRedirect(req.getContextPath().concat("/home/initialForm.htm"));
+//        }
+//
+//        if(user!=null){
+//            if(user.getUserEntity().getProfessionId()==1 && !req.getServletPath().startsWith("/user") ){
+//                res.sendRedirect(req.getContextPath().concat("/user/search.htm"));
+//            }
+//        }
 
-        if(user == null && !(req.getServletPath().startsWith("/home") || req.getServletPath().startsWith("/verification") || req.getServletPath().equals("/index.jsp"))) {
-            res.sendRedirect(req.getContextPath().concat("/home/initialForm.htm"));
-        }
-
-        if(user!=null){
-            if(user.getUserEntity().getProfessionId()==1 && !req.getServletPath().startsWith("/user") ){
-                res.sendRedirect(req.getContextPath().concat("/user/search.htm"));
+        if(req.getServletPath().startsWith("/verification") || req.getServletPath().startsWith("/home")){
+            if(user!=null){
+                if(user.getUserEntity().getProfessionId()==1) res.sendRedirect(req.getContextPath().concat("/user/search.htm"));
             }
+        }else if(req.getServletPath().startsWith("/user")){
+            if(user==null) res.sendRedirect(req.getContextPath().concat("/home/initialForm.htm"));
+            else if (user.getUserEntity().getProfessionId()!=1) System.out.println("Profess");
         }
 
         filterChain.doFilter(servletRequest, servletResponse);
