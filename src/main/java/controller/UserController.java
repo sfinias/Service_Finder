c62efc7fd5b415c7adf9792bf8411a0acb487354
Ioannis Sfinias <sfinias@hotmail.com>
2018-12-20 19:58:28 +0200

    Modified ajax (again) and the profnavbar

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,9 +1,6 @@
 package controller;
 
-import dao.ProfessionsDAOInterface;
-import dao.ServiceDAOInterface;
-import dao.UserDAOInterface;
-import dao.VerificationTokenDAOInterface;
+import dao.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -23,9 +20,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import utils.MailService;
 import validation.FormValids;
 
@@ -34,7 +29,6 @@ import javax.validation.Valid;
 import model.ProfessionsEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import validation.EditFormValids;
 import validation.PasswordFormValids;
